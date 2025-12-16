1625f81ffa8e3b2be77e86a69a00c6202af60ff9
dixie24 <rpamg255@gmail.com>
2025-12-16 23:52:39 +0600

    updated 2.3

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -212,3 +212,14 @@ CELERY_TIMEZONE = "Asia/Bishkek"
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER    
+EMAIL_USE_SSL = False
